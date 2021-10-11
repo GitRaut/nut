5b4d7a9caf11d07cc4b7b95feb48a2c74f5abe11
GitRaut <92181249+GitRaut@users.noreply.github.com>
2021-10-11 21:55:29 +0300

    Update null_count.cpp

--- null_count.cpp
+++ null_count.cpp
@@ -3,6 +3,12 @@
 int itc_null_count(long long number)
 {
     int result;
+    if(number < 0){
+        number = number * -1;
+    }
+    if(number == 0){
+        return 1;
+    }
     result = 0;
     while(number > 0){
         if(number % 10 == 0){
